4b4c0c05eb5b785df33c2e6e5408f381ae55dc86
MakoskiVictor <makoski.ed@gmail.com>
2023-09-19 10:21:25 -0300

    declarate hipertext component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HipertextComponent } from './components/hipertext/hipertext.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    HipertextComponent
   ],
   imports: [
     CommonModule
