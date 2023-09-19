7209230891f4e6650c80b0c3f26d245d435c49ca
MakoskiVictor <makoski.ed@gmail.com>
2023-09-19 10:18:00 -0300

    quit import navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent, SharedModule } from './modules/shared';
+import { SharedModule } from './modules/shared';
 
 @NgModule({
   declarations: [
