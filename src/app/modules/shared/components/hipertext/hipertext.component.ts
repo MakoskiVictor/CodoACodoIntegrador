5b69e97025fd14980bbaa721ad3f91699155b73e
MakoskiVictor <makoski.ed@gmail.com>
2023-09-19 21:04:40 -0300

    add Input selectClass

--- src/app/modules/shared/components/hipertext/hipertext.component.ts
+++ src/app/modules/shared/components/hipertext/hipertext.component.ts
@@ -8,6 +8,7 @@ import { Component, Input } from '@angular/core';
 export class HipertextComponent {
   @Input() hipertextWords: string = '';
   @Input() hipertextLink: string = '';
+  @Input() selectClass: string = 'linkersGray';
 
   /* constructor(text: string, url: string) {
     this.hipertextWords = text;
