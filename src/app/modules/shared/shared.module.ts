c2004ff6c3afe25343349be02e7dcf3a9bc14d4e
MakoskiVictor <makoski.ed@gmail.com>
2023-09-19 21:55:14 -0300

    declare ConfDetails Component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HipertextComponent } from './components/hipertext/hipertext.component';
+import { ConfDetailsComponent } from './components/conf-details/conf-details.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    HipertextComponent
+    HipertextComponent,
+    ConfDetailsComponent
   ],
   imports: [
     CommonModule
