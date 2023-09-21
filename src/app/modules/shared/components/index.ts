b9b35d4ecfc348cf4437c0cf2392970a6fec9dbe
MakoskiVictor <makoski.ed@gmail.com>
2023-09-21 09:01:19 -0300

    update barrel

--- src/app/modules/shared/components/index.ts
+++ src/app/modules/shared/components/index.ts
@@ -1,4 +1,7 @@
 export * from './button';
+export * from './card';
 export * from './conf-details';
 export * from './hipertext/hipertext.component';
 export * from './navbar';
+export * from './pills';
+export * from './speakers';
