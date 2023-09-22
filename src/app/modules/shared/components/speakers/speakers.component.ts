83be03de11903e47042e4b82f5ad3b4a070fd4ac
MakoskiVictor <makoski.ed@gmail.com>
2023-09-21 23:13:36 -0300

    add speakers info

--- src/app/modules/shared/components/speakers/speakers.component.ts
+++ src/app/modules/shared/components/speakers/speakers.component.ts
@@ -7,5 +7,30 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SpeakersComponent {
-
+  speakersInfo = [
+    {
+      image: 'assets/speakers/steve.jpg',
+      name: 'Steve Jobs',
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Minima possimus saepe deserunt velit quo aspernatur iure non 
+      cum voluptatum itaque, architecto dolorum dicta ullam aut, 
+      eos pariatur aliquam tenetur explicabo.`
+    },
+    {
+      image: 'assets/speakers/bill.jpg',
+      name: 'Bill Gates',
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Minima possimus saepe deserunt velit quo aspernatur iure non 
+      cum voluptatum itaque, architecto dolorum dicta ullam aut, 
+      eos pariatur aliquam tenetur explicabo.`
+    },
+    {
+      image: 'assets/speakers/ada.jpeg',
+      name: 'Ada Lovelace',
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Minima possimus saepe deserunt velit quo aspernatur iure non 
+      cum voluptatum itaque, architecto dolorum dicta ullam aut, 
+      eos pariatur aliquam tenetur explicabo.`
+    }
+  ]
 }
