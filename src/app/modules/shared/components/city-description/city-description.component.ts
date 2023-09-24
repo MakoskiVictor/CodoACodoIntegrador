87107232057aa370c91503f412815f7d2566a7d0
MakoskiVictor <makoski.ed@gmail.com>
2023-09-24 16:40:06 -0300

    setdescription, title and button props

--- src/app/modules/shared/components/city-description/city-description.component.ts
+++ src/app/modules/shared/components/city-description/city-description.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class CityDescriptionComponent {
 
+  title = 'Bs As -Octubre'
+  description = `Buenos Aires es la provincia y localidad más grande del estado de Argentina. 
+  En los Estados Unidos, Honolulu es la más sureña de entre las principales 
+  ciudades estadounidenses. Aunque el nombre de Honolulu se refiere al área urbana
+  en la costa de la isla de Oahu, la ciudad y el condado de Honalulu han formado 
+  una ciudad-condado consolidada que cubre toda la ciudad (aproximadamente 600 km² 
+  de superficie).`
+  btnClass: string = 'btn1'
+  btnText: string = 'Conocé más'
+
 }
