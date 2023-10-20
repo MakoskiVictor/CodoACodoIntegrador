3dcf6ab67c47007f7f294d0b8d4ec654cb5af7d4
MakoskiVictor <makoski.ed@gmail.com>
2023-10-20 17:55:09 -0300

    quit route module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './modules/shared';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = []
 
 @NgModule({
   declarations: [
@@ -15,8 +12,7 @@ const routes: Routes = []
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule,
-    RouterModule.forRoot(routes, { useHash: true })
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
