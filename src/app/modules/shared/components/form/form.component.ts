5bc43299713a578f564948fec26e7785d79f1828
MakoskiVictor <makoski.ed@gmail.com>
2023-10-25 18:57:09 -0300

    add calculate payment logic

--- src/app/modules/shared/components/form/form.component.ts
+++ src/app/modules/shared/components/form/form.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent {
+  ticketPrice = 200;
+  total = 0;
+
+  calculateTotalPayment (numberOfTickets: number, category: string) {
+    const ticketsCost = this.ticketPrice * numberOfTickets;
+
+    if (category === "estudiante") this.total = ticketsCost * 0.8;
+    if (category === "trainee") this.total = ticketsCost * 0.5;
+    if (category === "junior") this.total = ticketsCost * 0.15;
+  }
 
 }
